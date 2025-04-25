9483d166792a82f581b6a77573a2caa0c0d3e2f6
unqMAK <mandarak123@gmail.com>
2025-04-25 12:04:18 +0530

    updated

--- book-directory/server/src/server.js
+++ book-directory/server/src/server.js
@@ -9,7 +9,7 @@ const bookRoutes = require('./routes/bookRoutes');
 dotenv.config({ path: resolve(__dirname, '../.env') });
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 if (!process.env.MONGODB_URI) {
   console.error('No MongoDB URI found in environment variables');
